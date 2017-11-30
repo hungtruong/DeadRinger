0b227935d98b8dd7eac93402c762f2c01bdb4196
Hung Truong <hung@hung-truong.com>
2017-11-29 21:08:21 -0800

    Initial Commit

---
+++ iPhoneXLockScreen/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iPhoneXLockScreen
+//
+//  Created by Hung Truong on 11/29/17.
+//  Copyright © 2017 Hung Truong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
